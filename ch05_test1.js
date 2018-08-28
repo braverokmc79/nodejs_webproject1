f9355286504782eab2c85c4f262b6d680bcd8c2d
braverokmc79 <braverokmc79@gmail.com>
2018-08-28 19:54:23 +0900

    21 write
    writeHead

--- ch05_test1.js
+++ ch05_test1.js
@@ -2,7 +2,10 @@ var http =require('http');
 
 var server =http.createServer();
 
+var host='192.168.0.4';
+//var host='localhost';
 var port=3000;
-server.listen(port, function(){
-    console.log('웹서버가 실행되었습니다.'+ port);
+//50000 동시접속 수
+server.listen(port, host, '50000', function(){
+    console.log('웹서버가 실행되었습니다. ->'+ host+" : "+ port);
 });
