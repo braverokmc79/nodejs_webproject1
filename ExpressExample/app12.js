96d971d14d0679cefce5a9932869fe81c34e2410
braverokmc79 <braverokmc79@gmail.com>
2018-09-07 10:03:58 +0900

    32 세션관리 로그인 로그아웃 -
    32 세션관리 로그인 로그아웃 -

--- ExpressExample/app12.js
+++ ExpressExample/app12.js
@@ -5,12 +5,12 @@ var path=require('path');
 
 var bodyParser=require('body-parser');
 var cookieParser =require('cookie-parser');
+
+
 //세션 모듈 요청
-require('express-session');
-
-
-
+var session =require('express-session');
 var app=express();
+
 
 app.set('port', process.env.PORT || 3000);
 app.use('/public', static(path.join(__dirname, 'public')));
@@ -20,10 +20,8 @@ app.use(bodyParser.json());
 
 //쿠키파서
 app.use(cookieParser());
-
-
 //세션 저장
-app.use(expressSession({
+app.use(session({
     secret :"my key",
     resave:true,
     saveUninitialized:true
@@ -31,7 +29,7 @@ app.use(expressSession({
 
 
 var router =express.Router();
-
+ 
 
 router.route('/process/product').get(function(req, res){
    console.log('/process/product 라우팅 함수 호출됨.');
@@ -40,10 +38,53 @@ router.route('/process/product').get(function(req, res){
         res.redirect('/public/product.html');
     }else{
         res.redirect('/public/login2.html');
+    }   
+});
+
+router.route('/process/login').post(function(req, res){
+    console.log('/process/login 라우팅 함수 호출됨');
+    
+    var paramId=req.body.id || req.query.id;
+    var paramPassword=req.body.password || req.query.password;
+    console.log("요청 파라미터 : " + paramId+ ' , ' + paramPassword);
+    
+    if(req.session.user){
+        console.log('이미 로그인되어 있습니다');
+        res.redirect('/public/product.html');
+    }else{
+        req.session.user={
+            id:paramId,
+            name:"소녀시대",
+            authorized:true
+        };
+        
+        res.writeHead(200, {"Content-Type" :"text/html;charset=utf8"});
+        res.write('<h1>로그인 성공</h1>');
+        res.write('<p>Id : ' +paramId +  '</p>');
+        res.write('<br><br><a href="/process/product">상품 페이지로 이동하기</a>');
+        res.end();
     }
     
-    
 });
+
+router.route('/process/logout').get(function(req, res){
+    console.log('/process/logout 라우팅 함수 호출됨.');    
+    if(req.session.user){
+        console.log('로그아웃합니다.');
+        req.session.destroy(function(err){
+            if(err){
+                console.log('세션 삭제 시 에러 발생.');
+                return;
+            }            
+            console.log('세션 삭제 성공.');
+            res.redirect('/public/login2.html');            
+        });
+    }else{
+        console.log('로그인되어 있지 않습니다.');
+        res.redirect('/public/login2.html');
+    }    
+});
+
 
 
 router.route('/process/setUserCookie').get(function(req, res){
