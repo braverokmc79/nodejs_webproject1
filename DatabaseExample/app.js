8bd774e034dcbebcbec954b618317dc8d548a729
braverokmc79 <braverokmc79@gmail.com>
2018-09-11 09:29:14 +0900

    Nodejs Mongodb 연결 후 로그인 인증 - 38

--- DatabaseExample/app.js
+++ DatabaseExample/app.js
@@ -5,9 +5,28 @@ var path=require('path');
 
 var bodyParser=require('body-parser');
 var cookieParser=require('cookie-parser');
-var expressSession=require('express-session');
+var session=require('express-session');
 //에러 핸들러 모듈 사용
 var expressErrorHandler=require('express-error-handler');
+
+//mongodb 모듈 사용
+var MongoClient=require('mongodb').MongoClient;
+
+
+var database;
+//데이터베이스 연결
+function connectDB(){
+    var databaseUrl='mongodb://localhost:27017/local';
+    
+    MongoClient.connect(databaseUrl, { useNewUrlParser: true },function(err, db){
+        if(err){
+            console.log('데이터베이스 연결 시 에러 발생함');
+            return;
+        }   
+        console.log('데이터베이스에 연결됨 : ' + databaseUrl);
+        database=db.db('local'); /*database명을 명시했다.*/
+    });
+}
 
 var app=express();
 
@@ -15,24 +34,96 @@ app.set('port', process.env.PORT || 3000);
 
 //미들웨어 등록 시작
 app.use('/public', static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
-app.use(expressSession({
-    secret:'my key',
-    reserve:true,
-    saveUninitilized:true
-    
+
+//세션 저장
+app.use(session({
+    secret :"my key",
+    resave:true,
+    saveUninitialized:true
 }));
-
+ 
 //미들웨어 등록 끝
-
-
 var router =express.Router();
 
+router.route('/process/login').post(function(req, res){
+      console.log('/process/login 라우팅 함수 호출됨 ');
+      var paramId=req.body.id || req.query.id;
+      var paramPassword=req.body.password || req.query.password;
+    
+      console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+    
+      if(database){
+          console.log("DB 사용자 정보 인증 시작");
+          authUser(database, paramId, paramPassword, function(err, docs){
+              if(err) {
+                  console.log('에러 발생. ');
+                  res.writeHead(200, {"Content-Type": "text/html;charset=utf8"});
+                  res.write('<h1>에러 발생</h1>');
+                  res.end();                  
+              }
+              
+              if(docs){
+                  console.dir(docs);
+                  res.writeHead(200, {"Content-Type": "text/html;charset=utf8"});
+                  res.write('<h1>사용자 로그인 성공</h1>');
+                  res.write("<div><p>사용자 : " + docs[0].name +'</p></div>');
+                  res.write('<br><br><a href="/public/login.html">다시 로그인하기</a>');
+                  res.end();
+                  
+              }else{
+                  console.log('에러 발생. ');
+                  res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
+                  res.write('<h1>사용자 데이터 조회 안됨.</h1>');
+                  res.end();            
+              }
+          });
+      }else{
+
+            console.log('에러 발생. ');
+            res.writeHead(200, {"Content-Type" : "text/html;charset=utf8"});
+            res.write('<h1>데이터베이스 연결 안됨.</h1>');
+            res.end();
+                  
+      }  
+    
+});
+
+
 app.use('/', router);
+
+
+var authUser =function(db, id, password, callback){
+    console.log('authUser 호출됨. ');
+    console.dir(db);
+    var users=db.collection('users');
+    
+    console.dir(users);
+    
+    
+    users.find({"id":id,  "password":password}).toArray(function(err, docs){
+         if(err){
+            console.log('error');
+            callback(err, null);
+             
+         }
+            
+         if(docs.length >0){
+            
+            console.log('일치하는 사용자를 찾음.');
+             callback(null, docs);
+         }else{
+             console.log('일치하는 사용자를 찾지 못함.');
+             callback(null, null);
+         }  
+    });
+    
+    
+};
+
 
 //404 에러 페이지 처리
 var errorHandler=expressErrorHandler({
@@ -48,6 +139,9 @@ app.use(errorHandler);
 
 var server=http.createServer(app).listen(app.get('port'), function(){
    console.log('익스프레스로 웹 서버를 실행함 : ' + app.get('port')) ;
+   
+    connectDB();
+    console.dir(database);
 });
 
 
